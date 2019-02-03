eb694555262890a6852066fb1590310e4a3a0490
Aayushi Modi <modi.aayu@gmail.com>
2019-02-02 18:05:17 -0800

    add logic comments in the database file

--- YManage/src/org/ymca/tvc/ymanage/server/DB.java
+++ YManage/src/org/ymca/tvc/ymanage/server/DB.java
@@ -7,4 +7,65 @@ public class DB {
 	static DB getCurrent() {
 		return current; 
 	}
+	
+	/*
+	 *  Checkin Student
+	 *  
+	 *  Is there a meeting going on? 
+	 *  	No - return error
+	 *  
+	 *  Is there a student with the given name
+	 *  	No - return error
+	 *  
+	 *  Is the student already checked in in the current meeting?
+	 * 
+	 *  	Yes - return error already checked in
+	 * 
+	 *  Add to current meeting the attendance record for this student
+	 *  Add to the student the attendance record
+	 *  
+	 *  return the time at which the student is checked in.
+	 *  
+	 */
+	
+	/*
+	 * Start a meeting
+	 * 	Is there already meeting in progress 
+	 * 		Yes return error
+	 * 
+	 *  Create new current meeting
+	 *  Add record false to all students for this meeting
+	 *  
+	 * 		
+	 */
+	
+	/*
+	 * Stop a meeting 
+	 * 	Is there a meeting in progress
+	 * 		No return error
+	 * 
+	 * Take the current meeting and add it to the history
+	 * Reset the current meeting to null
+	 */
+	
+	/*
+	 * Get the current meeting 
+	 */
+	
+	/*
+	 * Add volunteer
+	 */ 
+	
+	/*
+	 * Remove volunteer
+	 */
+	
+	/*
+	 * Get volunteer
+	 */
+	
+	/*
+	 * Get all volunteers
+	 */
+	 
 }
